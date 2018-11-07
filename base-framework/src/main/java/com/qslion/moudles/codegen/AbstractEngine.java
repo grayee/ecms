98f1267ee7ebe8a02c22c8017cca218fa9a4a671
Gray.Z <zrg523@gmail.com>
2018-11-07 18:02:15 +0800

    update commit

--- base-framework/src/main/java/com/qslion/moudles/codegen/AbstractEngine.java
+++ base-framework/src/main/java/com/qslion/moudles/codegen/AbstractEngine.java
@@ -1,8 +1,15 @@
 package com.qslion.moudles.codegen;
 
+import com.baomidou.mybatisplus.toolkit.StringUtils;
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.qslion.framework.enums.ResultCode;
+import com.qslion.framework.exception.BusinessException;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import freemarker.template.TemplateExceptionHandler;
 import java.io.File;
 import java.io.IOException;
@@ -11,13 +18,14 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.joda.time.DateTime;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 import org.springframework.util.ResourceUtils;
 
 /**
@@ -30,15 +38,18 @@ public abstract class AbstractEngine implements CodeCreator {
 
     protected final Logger logger = LogManager.getLogger(this.getClass());
 
-    Configuration cfg;
-    Properties props;
+    private static final String FILE_FTL_EXT = ".ftl";
+    private static final String FILE_JAVA_EXT = ".java";
+
+    private Configuration cfg;
+    private Properties props;
 
     AbstractEngine() {
         try {
             //初始化FreeMarker配置 ,创建一个Configuration实例
             cfg = new Configuration(Configuration.VERSION_2_3_28);
             //模板加载器,Class.getResource()方法来找到模板,第二个参数是给模板的名称来加前缀的
-            cfg.setClassForTemplateLoading(getClass(), StringUtils.EMPTY);
+            cfg.setClassForTemplateLoading(getClass(), File.separator + "template");
             cfg.setDefaultEncoding(StandardCharsets.UTF_8.toString());
             cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
             cfg.setLogTemplateExceptions(false);
@@ -63,13 +74,36 @@ public abstract class AbstractEngine implements CodeCreator {
         return dataMap;
     }
 
-    Path prepareFile(String filePath) {
-        return Paths.get(filePath);
+    String getFilePath(String pkgNameKey, String fileNameKey, String... suffix) {
+        String packageName = props.getProperty(pkgNameKey);
+        String fileName = props.getProperty(fileNameKey) +
+            (suffix.length > 0 ? suffix[0] : StringUtils.EMPTY) + FILE_JAVA_EXT;
+        String rootPath = System.getProperty("user.dir");
+        List<String> paths = Lists.newArrayList();
+        paths.add(rootPath);
+        paths.add(props.get("projectName") + File.separator + "src"
+            + File.separator + "main" + File.separator + "java");
+        paths.addAll(Splitter.on(".").splitToList(packageName));
+        paths.add(fileName);
+        return Joiner.on(File.separator).join(paths);
     }
 
-    Template prepareTemplate(String templateName) throws IOException {
-        if (!".ftl".endsWith(templateName)) {
-            return null;
+    private Path prepareFile(String filePath) {
+        Path path = Paths.get(filePath);
+        try {
+            if (!Files.exists(path)) {
+                Files.createDirectories(path.getParent());
+                Files.createFile(path);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return path;
+    }
+
+    private Template prepareTemplate(String templateName) throws IOException {
+        if (!templateName.endsWith(FILE_FTL_EXT)) {
+            throw new BusinessException(ResultCode.FAIL);
         }
         return cfg.getTemplate(templateName);
     }
@@ -95,7 +129,19 @@ public abstract class AbstractEngine implements CodeCreator {
         }
     }
 
-    Properties loadProperties() throws Exception {
+    /**
+     * 写为字符
+     *
+     * @param templateName 模板名称
+     * @param dataMap 数据
+     * @return 字符串
+     */
+    String writeAsString(String templateName, Map<String, Object> dataMap) throws IOException, TemplateException {
+        Template template = prepareTemplate(templateName);
+        return FreeMarkerTemplateUtils.processTemplateIntoString(template, dataMap);
+    }
+
+    private Properties loadProperties() throws Exception {
         Properties props = new Properties();
         String fileName = "moduleConfig.properties";
         File configFile = ResourceUtils.getFile(String.format("classpath:%s", fileName));
--- base-framework/src/main/java/com/qslion/moudles/codegen/CodeGenerator.java
+++ base-framework/src/main/java/com/qslion/moudles/codegen/CodeGenerator.java
@@ -1,6 +1,5 @@
 package com.qslion.moudles.codegen;
 
-import java.io.File;
 import java.io.IOException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,69 +7,43 @@ import org.apache.logging.log4j.Logger;
 public class CodeGenerator extends AbstractEngine {
 
     protected final Logger logger = LogManager.getLogger(this.getClass());
-    //文件分隔符
-    public static final String SPT = File.separator;
-    //包名
-    private String packageName;
-    //文件名称
-    private String fileName;
 
-
-    public CodeGenerator(String packageName, String fileName) throws Exception {
+    private CodeGenerator() throws Exception {
         super();
-        this.packageName = packageName;
-        this.fileName = fileName;
-    }
-
-
-    private String getFilePath(String packageName, String name) {
-        String path = packageName.replaceAll("\\.", File.separator);
-        return String.format("src/%s/%s", path, name);
     }
 
     @Override
     public void generateEntity(String tableName) throws IOException {
-        String serviceFilePath = getFilePath(props.getProperty("entityPackageName"),
-            props.getProperty("entityClassName") + ".java");
+        String serviceFilePath = getFilePath("entityPackageName", "entityClassName");
         writeToFile("entity.ftl", serviceFilePath, getDataMap());
     }
 
     @Override
     public void generateDao(String tableName) throws IOException {
-        String serviceFilePath = getFilePath(props.getProperty("daoPackageName"),
-            props.getProperty("entityClassName") + "Repository.java");
+        String serviceFilePath = getFilePath("daoPackageName", "entityClassName", "Repository.java");
         writeToFile("repository.ftl", serviceFilePath, getDataMap());
     }
 
     @Override
     public void generateService(String tableName) throws IOException {
-        String serviceFilePath = getFilePath(props.getProperty("servicePackageName"),
-            props.getProperty("entityClassName") + "Service.java");
+        String serviceFilePath = getFilePath("servicePackageName", "entityClassName", "Service.java");
         writeToFile("service.ftl", serviceFilePath, getDataMap());
     }
 
     @Override
     public void generateServiceImpl(String tableName) throws IOException {
-        String serviceImplFilePath = getFilePath(props.getProperty("serviceImplPackageName"),
-            props.getProperty("entityClassName") + "ServiceImpl.java");
+        String serviceImplFilePath = getFilePath("serviceImplPackageName", "entityClassName", "ServiceImpl.java");
         writeToFile("serviceImpl.ftl", serviceImplFilePath, getDataMap());
     }
 
     @Override
     public void generateController(String tableName) throws IOException {
-        String serviceImplFilePath = getFilePath(props.getProperty("controllerPackageName"),
-            props.getProperty("entityClassName") + "Controller.java");
+        String serviceImplFilePath = getFilePath("controllerPackageName", "entityClassName", "Controller.java");
         writeToFile("controller.ftl", serviceImplFilePath, getDataMap());
     }
 
 
-    /**
-     * @param args
-     * @throws Exception
-     */
     public static void main(String[] args) throws Exception {
-        String packageName = "com.qslion.app";
-        String fileName = "moduleConfig.properties";
-        new CodeGenerator(packageName, fileName).generateAll("au_user");
+        new CodeGenerator().generateAll("au_user");
     }
 }
