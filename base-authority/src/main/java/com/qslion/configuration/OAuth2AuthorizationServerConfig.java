822d49f8d16b80742e53cbf05d297a73e846f884
Gray.Z <zrg523@gmail.com>
2018-11-28 16:44:57 +0800

    update commit

--- base-authority/src/main/java/com/qslion/configuration/OAuth2AuthorizationServerConfig.java
+++ base-authority/src/main/java/com/qslion/configuration/OAuth2AuthorizationServerConfig.java
@@ -1,5 +1,7 @@
 package com.qslion.configuration;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +20,10 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 /**
  * OAuth2 授权服务器配置
@@ -42,10 +46,22 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
     @Autowired
     private DataSource dataSource;
 
-    @Bean
+    @Autowired
+    private JwtAccessTokenConverter jwtAccessTokenConverter;
+
+    @Autowired
+    private TokenEnhancer jwtTokenEnhancer;
+
+    @Autowired
+    private TokenStore jwtTokenStore;
+
+
+
+  /*  @Bean
     public TokenStore tokenStore() {
         return new JdbcTokenStore(dataSource);
     }
+*/
 
     @Bean
     public ClientDetailsService clientDetails() {
@@ -53,8 +69,7 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
     }
 
     /**
-     * 配置客户端认证详情服务（ClientDetailsService）,定义客户端细节服务中的内存或 JDBC 实现
-     * 用户端为          :  （UserDetailsService）
+     * 配置客户端认证详情服务（ClientDetailsService）,定义客户端细节服务中的内存或 JDBC 实现 用户端为          :  （UserDetailsService）
      *
      * @param clients 客户端认证配置
      * @throws Exception 异常
@@ -65,8 +80,7 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
     }
 
     /**
-     * 配置认证（authorization）以及令牌（token）的访问端点和令牌服务(token services)
-     * 默认情况下，除了密码之外，所有的授权类型都是受支持
+     * 配置认证（authorization）以及令牌（token）的访问端点和令牌服务(token services) 默认情况下，除了密码之外，所有的授权类型都是受支持
      *
      * @param endpoints 端点配置 TokenStore，TokenGranter，OAuth2RequestFactory
      */
@@ -74,11 +88,20 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
         endpoints.allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST);
         //通过注入 AuthenticationManager 来开启密码授权
-        endpoints.authenticationManager(authenticationManager);
-        endpoints.tokenStore(tokenStore());
-        endpoints.userDetailsService(userDetailService);
+        endpoints.authenticationManager(authenticationManager)
+            .tokenStore(jwtTokenStore)
+            .userDetailsService(userDetailService);
         //endpoints.tokenStore(new RedisTokenStore(redisConnectionFactory))  --redis 存储TOKEN
         endpoints.tokenServices(defaultTokenServices());
+
+        TokenEnhancerChain enhancerChain = new TokenEnhancerChain();
+        List<TokenEnhancer> enhancerList = new ArrayList<>();
+        enhancerList.add(jwtTokenEnhancer);
+        enhancerList.add(jwtAccessTokenConverter);
+        enhancerChain.setTokenEnhancers(enhancerList);
+
+        endpoints.tokenEnhancer(enhancerChain)
+            .accessTokenConverter(jwtAccessTokenConverter);
     }
 
     /**
@@ -88,7 +111,7 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
     @Bean
     public DefaultTokenServices defaultTokenServices() {
         DefaultTokenServices tokenServices = new DefaultTokenServices();
-        tokenServices.setTokenStore(tokenStore());
+        tokenServices.setTokenStore(jwtTokenStore);
         tokenServices.setSupportRefreshToken(true);
         tokenServices.setClientDetailsService(clientDetails());
         // token有效期自定义设置，默认12小时
@@ -100,10 +123,8 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
 
 
     /**
-     * 配置令牌端点(Token Endpoint)的安全约束
-     * 配置AuthorizationServer安全认证的相关信息，创建ClientCredentialsTokenEndpointFilter核心过滤器,
-     * 经过ClientCredentialsTokenEndpointFilter之后，身份信息已经得到了AuthenticationManager的验证。
-     * 接着便到达TokenEndpoint
+     * 配置令牌端点(Token Endpoint)的安全约束 配置AuthorizationServer安全认证的相关信息，创建ClientCredentialsTokenEndpointFilter核心过滤器,
+     * 经过ClientCredentialsTokenEndpointFilter之后，身份信息已经得到了AuthenticationManager的验证。 接着便到达TokenEndpoint
      *
      * @param oauthServer 授权服务配置
      */
