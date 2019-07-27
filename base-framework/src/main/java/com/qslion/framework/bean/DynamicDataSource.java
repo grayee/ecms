481dc2e8b9f2186bd141e32312e9e1cf00f1dd19
Gray.Z <zrg523@gmail.com>
2019-07-27 21:50:53 +0800

    update commit

--- base-framework/src/main/java/com/qslion/framework/bean/DynamicDataSource.java
+++ base-framework/src/main/java/com/qslion/framework/bean/DynamicDataSource.java
@@ -1,10 +1,10 @@
 package com.qslion.framework.bean;
 
+import java.util.Map;
+import javax.sql.DataSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
-
-import javax.sql.DataSource;
 
 /**
  * DynamicDataSource 动态数据源
@@ -15,6 +15,12 @@ import javax.sql.DataSource;
 public class DynamicDataSource extends AbstractRoutingDataSource {
 
     protected final Logger logger = LogManager.getLogger(this.getClass());
+
+    public DynamicDataSource(DataSource defaultTargetDataSource, Map<Object, Object> targetDataSources) {
+        super.setDefaultTargetDataSource(defaultTargetDataSource);
+        super.setTargetDataSources(targetDataSources);
+        super.afterPropertiesSet();
+    }
 
     @Override
     protected Object determineCurrentLookupKey() {
--- base-framework/src/main/java/com/qslion/framework/configuration/DataSourceConfig.java
+++ base-framework/src/main/java/com/qslion/framework/configuration/DataSourceConfig.java
@@ -1,27 +1,21 @@
 package com.qslion.framework.configuration;
 
+import com.google.common.collect.Maps;
 import com.qslion.framework.bean.DataSourceContextHolder;
 import com.qslion.framework.bean.DynamicDataSource;
+import java.util.Map;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
-
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * DataSource配置类
@@ -38,7 +32,6 @@ public class DataSourceConfig {
     private final static String SLAVE_DATASOURCE_KEY = "slaveDataSource";
 
     @Bean(name = MASTER_DATASOURCE_KEY)
-    @Primary
     @ConfigurationProperties(prefix = "spring.datasource.master")
     public DataSource masterDataSource() {
         logger.info("init master datasource...");
@@ -53,42 +46,32 @@ public class DataSourceConfig {
         return DataSourceBuilder.create().build();
     }
 
-  /*  @Bean
+    @Bean
     @Primary
-    public AbstractRoutingDataSource routingDataSource(DataSource masterDataSource, DataSource slaveDataSource) {
-        DynamicDataSource dds = new DynamicDataSource();
-        dds.setDefaultTargetDataSource(DataSourceContextHolder.DataSourceType.MASTER.name());
-        Map<Object, Object> targetDataSources = new HashMap<>();
+    public DynamicDataSource routingDataSource(DataSource masterDataSource, DataSource slaveDataSource) {
+        Map<Object, Object> targetDataSources = Maps.newHashMap();
         targetDataSources.put(DataSourceContextHolder.DataSourceType.MASTER.name(), masterDataSource);
         targetDataSources.put(DataSourceContextHolder.DataSourceType.SLAVE.name(), slaveDataSource);
-        dds.setTargetDataSources(targetDataSources);
-        return dds;
+        return new DynamicDataSource(masterDataSource, targetDataSources);
     }
 
-    *//**
-     * 实体文件
-     * @return
-     *//*
     @Bean(name = "entityManagerFactory")
     public LocalContainerEntityManagerFactoryBean emf(DataSource masterDataSource, DataSource slaveDataSource) {
         LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
-        emf.setDataSource(routingDataSource(masterDataSource,slaveDataSource));
+        emf.setDataSource(routingDataSource(masterDataSource, slaveDataSource));
         //这里一定要是你自己App实体类的位置
-        emf.setPackagesToScan(new String[]{"com.qslion.*.model"});
+        emf.setPackagesToScan(new String[]{"com.qslion.**.entity"});
         emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
         return emf;
     }
 
-    *//**
-     * 事务关管理
-     * @return
-     *//*
+
     @Bean(name = "transactionManager")
     public JpaTransactionManager transactionManager(DataSource masterDataSource, DataSource slaveDataSource) {
-        JpaTransactionManager tm =new JpaTransactionManager();
-        EntityManagerFactory e=emf(masterDataSource,slaveDataSource).getNativeEntityManagerFactory();
+        JpaTransactionManager tm = new JpaTransactionManager();
+        EntityManagerFactory e = emf(masterDataSource, slaveDataSource).getNativeEntityManagerFactory();
         tm.setEntityManagerFactory(e);
         return tm;
-    }*/
+    }
 
 }
