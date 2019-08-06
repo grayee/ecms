a6df02d1831df69c2e5d92a5a406ffd4a7cbdfb2
gray <gray@google.com>
2019-08-06 18:13:32 +0800

    update commit

--- ecms-web/src/main/java/com/qslion/Startup.java
+++ ecms-web/src/main/java/com/qslion/Startup.java
@@ -17,11 +17,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.util.ResourceUtils;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.request.RequestContextListener;
 @EnableDiscoveryClient
 @EnableJpaAuditing
@@ -32,6 +34,12 @@ public class Startup {
     public static void main(String[] args) throws Exception {
         System.setProperty("nacos.logging.default.config.enabled","false");
         SpringApplication.run(Startup.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
     @Bean
--- ecms-web/src/main/java/com/qslion/controller/BookController.java
+++ ecms-web/src/main/java/com/qslion/controller/BookController.java
@@ -4,15 +4,18 @@ import com.qslion.entity.Book;
 import com.qslion.service.BookService;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -26,12 +29,20 @@ import javax.servlet.http.HttpServletResponse;
 public class BookController {
     private BookService bookService;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping(method = RequestMethod.GET)
     public Book get(String bookName) {
         if (StringUtils.isEmpty(bookName)) {
             bookName = "hello world";
         }
         return bookService.get(bookName);
+    }
+
+    @RequestMapping(value = "/author",method = RequestMethod.GET)
+    public String getAuthor() {
+        return restTemplate.getForObject("http://ecms-auth/au/user/detail/1?access_token=6ea2e741-cdd9-4244-98f8-66609cc978a1", Map.class).toString();
     }
 
     @RequestMapping(method = RequestMethod.POST)
