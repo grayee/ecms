3d226c03c51f6b74a4fc6413c774eeac4a17de47
Gray.Z <zrg523@gmail.com>
2019-08-29 22:19:01 +0800

    update commit

--- base-framework/src/main/java/com/qslion/framework/configuration/WebMvcConfig.java
+++ base-framework/src/main/java/com/qslion/framework/configuration/WebMvcConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
